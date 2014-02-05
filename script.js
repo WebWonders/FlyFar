08d0b6c60896b006b94d60d7cb3248c66228ee67
Alex <apeggs2000@users.noreply.github.com>
2014-02-04 19:33:14 -0800

    Lots of additions
    All timer and score code bits un-commented and ready for use
    Added comments to show ends of functions

--- script.js
+++ script.js
@@ -17,7 +17,7 @@
       
       function counter() {
             this.Math.floor(Math.random() * 2);
-      }
+      } //end counter()
       
       function Plane() {
         tPlane = new Sprite(scene, "Images/RetroPlane.png", 50, 50);
@@ -28,7 +28,8 @@
           this.setPosition(scene.getMouseX(), 500);
         }
         return tPlane;
-      }
+      } //end Plane()
+      
       function Battery() {
         tBattery = new Sprite(scene, "Images/BatteryPic.png", 150, 100);
       tBattery.reset = function(){
@@ -36,7 +37,7 @@
             this.setDX((Math.random() * 10) - 5);
             newX = Math.random() * scene.width;
             this.setPosition(newX, 50);
-        } // end reset
+        }
         
         tBattery.checkBounds = function(){
             if (this.y > scene.height){
@@ -47,19 +48,20 @@
         tBattery.reset();
         
         return tBattery;
-    }
+    } //end Battery()
+    
     function makeBatteries(){
         batteries = new Array(NUM_BATTERIES);
         for (i = 0; i < NUM_BATTERIES; i++){
             batteries[i] = new Battery();
         }
-    }
+    } //end makeBatteries()
     
     function updateBatteries(){
         for (i = 0; i < NUM_BATTERIES; i++){
             batteries[i].update();
         }
-    }
+    } //end updateBatteries
     
     function Background(){
         tBackground = new Sprite(scene, "Images/OverheadGroundPic.jpg", 800, 1440);
@@ -72,37 +74,38 @@
             }
         }
         return tBackground;
-     }
+     } //end Background()
     
       function init() {
         scene = new Scene();
         scene.hideCursor();
-        //timePut = document.getElementById('timeLeft');
-        //flyTime = new Timer();
-        //flyTime.reset();
-        //scoreTime = new Timer();
-        //scoreTime.reset();
+        timePut = document.getElementById('timeLeft');
+        flyTime = new Timer();
+        flyTime.reset();
+        scoreTime = new Timer();
+        scoreTime.reset();
         plane = new Plane();
         makeBatteries();
         background = new Background();
         //findScore();
         scene.start();
-      }
+      } //end init()
+      
       function update() {
         scene.clear();
         plane.followMouse();
         background.update();
         plane.update();
         updateBatteries();
-        //currentTime = timer.getElapsedTime();
-        //timePut.innerHTML = currentTime;
-    }
-/*    function reset() {
+        currentTime = timer.getElapsedTime();
+        timePut.innerHTML = currentTime;
+    } //end update()
+    function reset() {
         flyTime.reset();
         scoreTime.reset();
         alert("Your score was" + Math.floor(userScore));
     }
     function findScore() {
         userScore = scoreTime.getElapsedTime();
-    }*/
+    }
 
